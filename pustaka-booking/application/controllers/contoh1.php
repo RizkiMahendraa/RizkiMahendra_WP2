0e54831bd923c5a3c6a37248b1fce025a8a776fe
RizkiMahendraa <mahendrarizky81@gmail.com>
2021-10-28 12:28:01 +0700

    push file project latihan

---
+++ pustaka-booking/application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Hallo Perkenalkan</h1>";
+        echo "Nama saya Rizki Mahendra
+Saat ini saya sedang berkuliah di Bina Sarana Informatika Bogor
+dan Saya sedang tinggal di daerah Ciomas
+olah raga yang saya sukai saat ini adalah
+Bulutangkis dan Basket";
+    }
+}
